73354573d884b781a3ff4a2b279cd23f125665e7
feng <feng@xatyu.cn>
2019-06-24 14:07:25 +0800

    数据库连接及参数校验

--- src/main/java/com/tinyu/demo/SpringbootDemoApplication.java
+++ src/main/java/com/tinyu/demo/SpringbootDemoApplication.java
@@ -1,17 +1,15 @@
 package com.tinyu.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+//指定主类启动时扫描包 【默认仅仅扫描主类所在包及其子包】@component标注
+@SpringBootApplication(scanBasePackages={"com.tinyu.demo.**"})   
 @EnableSwagger2
 public class SpringbootDemoApplication {
-
-	
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
--- src/main/java/com/tinyu/demo/controller/Hello.java
+++ src/main/java/com/tinyu/demo/controller/Hello.java
@@ -1,14 +1,47 @@
 package com.tinyu.demo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 @RestController
 public class Hello {
+	/**
+	 * 测试一  输出
+	 * @return
+	 */
 	@ApiOperation(value = "测试", notes = "测试swagger功能")
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String say() {
 		return "hello world!";
 	}
+	
+	/*
+	 * 测试二  传参
+	 */
+	@ApiOperation(value = "接受", notes = "输出对话内容")
+	@ApiImplicitParam(name="title", value = "对话标题", dataType = "string", required = true)
+	@RequestMapping(value = "listen", method = RequestMethod.POST)
+	public String listen(String title) {
+		return title;
+	}
+
+	@ApiOperation(value = "登录", notes = "系统登陆")
+	@ApiImplicitParams({
+		@ApiImplicitParam(name = "username", value = "用户名", dataType = "string", paramType="query"),
+		@ApiImplicitParam(name = "password", value = "密    码", dataType = "string", paramType="query")
+	})
+	@RequestMapping(value = "login", method = {RequestMethod.POST,RequestMethod.GET})
+	public String login(HttpServletRequest req) {
+		String pwd=req.getParameter("password");
+		String uname=req.getParameter("username");
+		return uname+","+pwd;
+	}
+		
 }
